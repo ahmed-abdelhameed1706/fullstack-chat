0d2b314de52fe0297e941e9dbf85511f4d225146
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-06-16 02:41:00 +0300

    configuration for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
+import path from "path";
 
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json());
@@ -16,9 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   console.log("Server is running on Port ", PORT);
 });
 
-// TODO: Add socket to the server
 // TODO: Configure this server for deployment
